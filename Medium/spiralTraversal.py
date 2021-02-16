c4c20077016d3dcd5f38cac9fee48984563029dd
System Administrator <mppk2009@gmail.com>
2021-02-15 21:30:58 -0700

    Added spiralTraversal.py

---
+++ Medium/spiralTraversal.py
@@ -0,0 +1,46 @@
+class Solution:
+    def spiralTraverse(self, array):
+
+        # Time O(n) || Space O(n)
+        result = []
+        startRow, startCol = 0, 0
+        endRow, endCol = len(array) - 1, len(array[0]) - 1
+
+        while startRow <= endRow and startRow <= endCol:
+
+            for col in range(startCol, endCol + 1):
+                result.append(array[startRow][col])
+
+            for row in range(startRow + 1, endRow + 1):
+                result.append(array[row][endCol])
+
+            for col in reversed(range(startCol, endCol)):
+
+                ## To handle cases where there are single row
+                if startRow == endRow:
+                    break
+                result.append(array[endRow][col])
+
+            for row in reversed(range(startRow + 1, endRow)):
+
+                ## To handle cases where there are single column
+                if startCol == endCol:
+                    break
+                result.append(array[row][startCol])
+
+            startRow += 1
+            startCol += 1
+            endRow -= 1
+            endCol -= 1
+
+        return result
+
+
+if __name__ == "__main__":
+
+    print(
+        Solution().spiralTraverse(
+            [[1, 2, 3, 4], [12, 13, 14, 5], [11, 16, 15, 6], [10, 9, 8, 7]]
+        )
+    )
+    print(Solution().spiralTraverse([[1, 2, 3], [8, 9, 4], [7, 6, 5]]))
