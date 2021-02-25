3a7904b0f2d5db46a78b80e395c7eef9b7188956
System Administrator <mppk2009@gmail.com>
2021-02-24 21:22:45 -0700

    Updated validateBST

--- Medium/validateBST.py
+++ Medium/validateBST.py
@@ -1,30 +1,27 @@
 class Solution:
-
-	def __init__(self):
-    	self.currVal = float('-inf')
+    def __init__(self):
+        self.currVal = float("-inf")
 
     def validateBst1(self, tree):
-        return self.helper(tree, float('-inf'), float('inf'))
-
+        return self.helper(tree, float("-inf"), float("inf"))
 
     def helper(self, tree, minVal, maxVal):
-        
+
         if tree is None:
-            return True 
+            return True
         if tree.value < minVal or tree.value >= maxVal:
-            return False 
+            return False
         leftIsValid = self.helper(tree.left, minVal, tree.value)
         return leftIsValid and self.helper(tree.right, tree.value, maxVal)
 
-    
-	def validateBst2(self, root):
+    def validateBst2(self, root):
 
-		if root:
-			self.validateBst2(root.left)
-			if root.value >= self.currVal:
-				self.currVal = root.value
-			else:
-				return False 
-			self.validateBst2(root.right)
-    
-    	return True
+        if root:
+            self.validateBst2(root.left)
+            if root.value >= self.currVal:
+                self.currVal = root.value
+            else:
+                return False
+            self.validateBst2(root.right)
+
+        return True
