bbd00c611c6449828076e5e8971af5d34c16f758
System Administrator <mppk2009@gmail.com>
2021-03-03 00:54:38 -0700

    Added third solution to squared sorted array problem

--- Easy/sortedSquareArray.py
+++ Easy/sortedSquareArray.py
@@ -3,7 +3,7 @@ class Solution:
     ##SOLUTION 1:
     def sortedSquaredArray1(self, array):
 
-        # Time O(n) || Space O(n)
+        # Time O(nlogn) || Space O(n)
         if not array:
             return array
         if len(array) == 1:
@@ -20,6 +20,32 @@ class Solution:
 
     ##SOLUTION 2:
     def sortedSquaredArray2(self, array):
+        # Time O(n) || Space O(n)
+        if not array:
+            return array
+
+        sortedSquares = [0] * len(array)
+        left = 0
+        right = len(array) - 1
+
+        for idx in reversed(range(0, len(array))):
+
+            leftSquare = array[left] ** 2
+            rightSquare = array[right] ** 2
+
+            if leftSquare >= rightSquare:
+                sortedSquares[idx] = leftSquare
+                left += 1
+
+            else:
+                sortedSquares[idx] = rightSquare
+                right -= 1
+
+        return sortedSquares
+
+    ##SOLUTION 3:
+    def sortedSquaredArray3(self, array):
+
         # Time O(nlogn) || Space O(1)
         if not array:
             return array
@@ -43,4 +69,5 @@ class Solution:
 if __name__ == "__main__":
 
     print(Solution().sortedSquaredArray1([1, 2, 3, 5, 6, 8, 9]))
-    print(Solution().sortedSquaredArray1([-10, -5, 0, 5, 10]))
+    print(Solution().sortedSquaredArray2([-10, -5, 0, 5, 10]))
+    print(Solution().sortedSquaredArray3([-1, -2, -3, -4, -5]))
