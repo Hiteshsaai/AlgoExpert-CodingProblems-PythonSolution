db0c77ce647aa276124f11fc821f774c40878261
Hitesh Saai Mananchery Panneerselvam <mppk2009@gmail.com>
2021-04-16 21:03:55 -0600

    Added find loop node in LinkedList

---
+++ Hard/findLoopNodeLinkedList.py
@@ -0,0 +1,28 @@
+class Solution:
+
+    # This is an input class. Do not edit.
+    class LinkedList:
+        def __init__(self, value):
+            self.value = value
+            self.next = None
+
+    def findLoop(head):
+        ## Time O(n) || Space O(1)
+        if head:
+            node1, node2 = head, head
+            slow = head.next
+            fast = head.next.next
+            while True:
+                if slow == fast:
+                    break
+                else:
+                    slow = slow.next
+                    fast = fast.next.next
+
+            slow = head
+            while True:
+                if slow == fast:
+                    return slow
+                else:
+                    slow = slow.next
+                    fast = fast.next
